f70b38a5d164f83398be62b48e24d35394cb2abe
Rémy Bin <remy.bin@gmail.com>
2015-07-10 14:42:29 +0200

    Add Comparison and Subsumption function

--- Etude/checkResult.py
+++ Etude/checkResult.py
@@ -5,48 +5,42 @@
 '''
 
 '''
-def numeric_compare(x, y):
-        return x - y
 >>> sorted([5, 2, 4, 1, 3], cmp=numeric_compare)
 [1, 2, 3, 4, 5]
-'''
-'''
-   def __cmp__(self,other):
-        
 cmp(x, y)
-
     Compare the two objects x and y and return an integer according
     to the outcome. The return value is negative if x < y,
     zero if x == y and strictly positive if x > y.
-
-return cmp(self.count,other.count)
 '''
 
 
 class Cnf :
     '''
-    a Cnf is a disjonction of one or more Propositions or Pred (mixed here 
+    a Cnf is a disjonction of one or more Propositions or Pred
     the Cnf arg is a list
+    on ne vaut pas trier les CNF (ou alors pour optimiser mais seulement pouvoir 
+    verifier egalite et subsumption entre elles et pour cela ce sont ses arguments qui sont triés)
     '''
     def __init__(self,args):
         self.args = args 
-        #creer propre fonction de comparaison
+        
     def subsumes(self,cnf):
         for obj1 in self.args :
+            is_in = False
             for obj2 in cnf.args :
-                if 
-            
+                if obj1.is_eq(obj2):
+                    is_in = True
+                    break
+            if is_in == False:
+                return False
+        return True
         
     def is_subsumed(self, cnf):
-        pass
+        cnf.subsumes(self)
 
 '''
 QUESTION : EST CE QU 'ono peut AVOIR DES PROP ET DES PRED DANS UNE MEME Cnf ???
 REGARDER DANS LES FICHIERS DE REPONSES 
-
-mETTONS QU ON SUPPOSE QUE OUI :
-    UNE CNF AVEC PLUS DE PROP EST < UNE CNF AVEC MOINS.(for en iterant sur les 2 en meme temps)
-    ecrire algo sur papier
 '''
 
 class Prop():
@@ -56,10 +50,20 @@ class Prop():
         return True
     def is_pred(self):
         return False
+    def is_eq(self, obj):
+        if obj.is_prop() != self.is_prop() or obj.is_pred() != self.is_pred() :
+            return False
+        if self.name() != obj.name:
+            return False
+        else :
+            return True
 
 class Pred():
     '''
         prend un nombre variable d'arguments
+        definition de PROLOG        
+        Two clauses belong to the same predicate if they have the same functor (name) and the same arity (number of arguments).
+        !!  : on ne regarde pas le nom des variables ici
     '''
     def __init__(self,name, variables):
         self.name = name
@@ -68,9 +72,30 @@ class Pred():
         return False
     def is_pred(self):
         return True
-    pass
+    def is_eq(self, obj):
+        if obj.is_prop() != self.is_prop() or obj.is_pred() != self.is_pred() :
+            return False
+        if self.name() != obj.name:
+            return False
+        if len(self.variables) != len(obj.variables):
+            return False
+        else :
+            return True
+        
+def comp_All(obj1, obj2):
+    '''
+    Compare deux objets avec comme ordre :
+    prop < pred
+    et si type egal, compare les noms 
+    '''  
+    if obj1.is_prop() and obj2.is_pred() :
+        return -1
+    if obj1.is_pred() and obj2.is_prop():
+        return 1
+    if (obj1.is_prop() and obj2.is_prop) or ( obj1.is_pred() and obj2.is_pred()):
+        return comp_Name(obj1,obj2)    
 
-def comp_NAME(obj1, obj2):
+def comp_Name(obj1,obj2):    
     if obj1.name < obj2.name :
         return -1
     elif obj1.name > obj2.name :
@@ -78,20 +103,30 @@ def comp_NAME(obj1, obj2):
     else : 
         return 0
     
-def comp_PRED(pred1, pred2):
+def comp_Pred(pred1, pred2):
+    '''
+    ici on trie les predicats de même nom par le nombre d'arguments
+    ceux qui en ont le moins se retrouvent en premier
+    '''
     if pred1.name < pred2.name :
         return -1
     elif pred1.name > pred2.name :
         return 1
-    else :         
-        return comp_VARS(pred1.variables, pred2.variables)
+    #NAME EGAL
+    elif len(pred1.variables) < len(pred2.variables):
+        return -1
+    elif len(pred1.variables) > len(pred2.variables):
+        return 1      
+    #NBRE ARGS EGAL
+    else:
+        return comp_Vars(pred1, pred2)
         
-def comp_VARS(l1,l2):
+def comp_Vars(pred1,pred2):
     '''
-    un prédicat a toujours le mêm nombre d'arguments ?
-    On le suppose ici,
-    il faudra surement changer
+    PRECOND : même taille de liste, même nom
     '''
+    l1 = pred1.variables
+    l2 = pred2.variables
     for e1,e2 in zip(l1,l2) :
         if e1==e2 :continue
         if e1 < e2 :return -1
@@ -126,7 +161,7 @@ def algo_TRI_Cnf(cnfs):
         pour chaque Cnf : on coupe la queue à partir du premier predicat. (trié dans ordre propr < pred)
         et pour chaque queue, on numérote dans l'ordre d'apparition les VAR et on les renomme (type: int)
         on sorte ensuite la liste des VARs (arg) (sort numérique simple)
-        on sorte ensuite les pred avec COMP_PRED(pred1,pred2) ici (qui utilise COMP_VARS)
+        on sorte ensuite les pred avec comp_Pred(pred1,pred2) ici (qui utilise comp_Vars)
             if compLexicogrpahique(pred1.name,pred2.name) < or > return res 
             |REDONDANT ICI il suffit de trier les sous-listes de prédicats de même nom (forcéement contigu) mais plus simple de faire ca = si probleme de perf on verra
             else (==)
@@ -146,7 +181,7 @@ def algo_TRI_Cnf(cnfs):
         OPTIMISER en faisant ligne par ligne entraabillant sur le fichier...
     '''
     for c in cnfs:  
-        c.sort(cmp=comp_NAME)# not this because this makes a copy. sorted(cl, cmp=comp_NOM)
+        c.sort(cmp=comp_All)# not this => because this makes a copy. sorted(cl, cmp=comp_NOM)
         
         props=[]
         preds=[]
@@ -169,7 +204,7 @@ def algo_TRI_Cnf(cnfs):
             p.variables.sort()#comparaison numerique standard 
             print p.variables 
             
-        preds.sort(cmp=comp_PRED)
+        preds.sort(cmp=comp_Pred)
     
 def algo_CHECK_RESULTS():
 #    on a deux listes de CNF TTRIEes t onc ompar
