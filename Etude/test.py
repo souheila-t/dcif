2d1fb4c59014743473aac9a61e851580e9c26ecd
Rémy Bin <remy.bin@gmail.com>
2015-07-15 17:00:40 +0200

    Add MonoAgent&distSuffix Args

--- Etude/test.py
+++ Etude/test.py
@@ -5,6 +5,9 @@
 
 
 import sys
+from commons import *
+from subprocess import *
+
 PROJECT_PATH = "/home/magma/projects/dcif/Etude/"
 RSRC_PATH = PROJECT_PATH + 'ressources/'
 GEN_PATH = PROJECT_PATH + 'gen/'
@@ -14,120 +17,59 @@ BUILDGRAPH_JAR = TOOLS_PATH + "buildGraph.jar"
 KMETIS_EX = TOOLS_PATH + "metis-4.0.3/kmetis"
 GRAPH2DCF_JAR = TOOLS_PATH + "graph2dcf.jar"
 MAKESOLVARIANT_JAR = TOOLS_PATH + "makeSolVariant.jar"
-CFLAUNCHER_JAR =RSRC_PATH + 'CFLauncher_4.jar'
-
-
-from subprocess import *
-def jarWrapper(*args):
-    '''    
-        http://stackoverflow.com/questions/7372592/python-how-can-execute-a-jar-file-through-a-python-script
-        RQ : *args signifie ici un nombre variable d'arguments
-    '''
-    print list(args)
-    process = Popen(['java', '-jar']+list(args), stdout=PIPE, stderr=PIPE)
-    ret = []
-    while process.poll() is None:
-        line = process.stdout.readline()
-        if line != '' and line.endswith('\n'):
-            ret.append(line[:-1])
-    stdout, stderr = process.communicate()
-    ret += stdout.split('\n')
-    if stderr != '':
-        ret += stderr.split('\n')
-    ret.remove('')
-    print ret
-    return ret
-def exWrapper(*args):
-    print args
-    process = Popen(list(args), stdout=PIPE, stderr=PIPE)
-    ret = []
-    while process.poll() is None:
-        line = process.stdout.readline()
-        if line != '' and line.endswith('\n'):
-            ret.append(line[:-1])
-    stdout, stderr = process.communicate()
-    ret += stdout.split('\n')
-    if stderr != '':
-        ret += stderr.split('\n')
-    ret.remove('')
-    print ret
-    return ret
+CFLAUNCHER_JAR =RSRC_PATH + 'CFLauncher_5_csq.jar'
 
 
 
 import argparse
-parser = argparse.ArgumentParser(description='Process to test the DCIF.')
-parser.add_argument('infile', 
-                    help='input file with ext .sol \n For better project \
-                    organization, it is recommended to put the .sol file in \
-                    the ressources folder and to add the raltive path as an \
-                    argumeent such as ressrouces/mock.sol')
-                    
-parser.add_argument('outfile', 
-                    help='output file for the debug (ext .csv will be added)')
-
-parser.add_argument('-m', '--method', default='DICF-PB-Async',
-                    help='method used for DCIF : Async,  token, or star-based\
-                    \nchoices=[DICF-PB-Async, DICF-PB-Token, DICF-PB-Star\]')
-
-parser.add_argument('-v', '--verbose', action='store_true')
-
-parser.add_argument('-n', '--numagent',type=int , required=True,
-                    help = 'an integer for the number of agents on which it is\
-                    distributed')
-
-parser.add_argument('-t', '--timeout',type=int , 
-                    help = 'an integer for the timeout')
-
-parser.add_argument('--var', 
-                    help = 'var=varSuffix  use the variant with given suffix \
-                    (should begin by _).')
-
-parser.add_argument('--par', choices=['naive_eq', 'naive_indent','kmetis_hybrid','kmetis'], default = 'kmetis',
-                    help = 'type de partitionnement \n\
-                    naive_eq= divise les clauses equitablement (ne regarde pas si top_clauses ou axiom) \n\
-                    naive_indent= divise les clauses selon l indentation du fichier sol d origine (pareil que dessus)\n\
-                    eq_par= répartit équitablement ET les axiomes ET les Top_clauses
-                    kmetis_hybrid
-                    ,kmetis)
+def define_arguments():    
+    parser = argparse.ArgumentParser(description='Process to test the DCIF.')
+    parser.add_argument('infile', 
+                        help='input file with ext .sol \n For better project \
+                        organization, it is recommended to put the .sol file in \
+                        the ressources folder and to add the raltive path as an \
+                        argumeent such as ressrouces/mock.sol')
+                        
+    parser.add_argument('outfile', 
+                        help='output file for the debug (ext .csv will be added)')
     
-
-
+    parser.add_argument('-m', '--method', default='DICF-PB-Async',
+                        help='method used for DCIF : Async,  token, or star-based\
+                        \nchoices=[DICF-PB-Async, DICF-PB-Token, DICF-PB-Star\]')
+    
+    parser.add_argument('-v', '--verbose', action='store_true')
+    
+    parser.add_argument('-n', '--numagent',type=int ,
+                        help = 'an integer for the number of agents on which it is\
+                        distributed')
+    
+    parser.add_argument('-t', '--timeout',type=int , 
+                        help = 'an integer for the timeout')
+    
+    parser.add_argument('--var', 
+                        help = 'var=varSuffix  use the variant with given suffix \
+                        (should begin by _).')
+    
+    parser.add_argument('--par', choices=['naive_eq', 'naive_indent','kmetis_hybrid','kmetis'], default = 'kmetis',
+                        help = 'type de partitionnement \n\
+                        naive_eq= divise les clauses equitablement (ne regarde pas si top_clauses ou axiom) \n\
+                        naive_indent= divise les clauses selon l indentation du fichier sol d origine (pareil que dessus)\n\
+                        eq_par= répartit équitablement ET les axiomes ET les Top_clauses\
+                        kmetis_hybrid\
+                        ,kmetis')
+    parser.add_argument('--dist',
+                        help = 'distSuffix  use the distribution with given suffix (should begin by \"_\").");')
+        
+    return parser
+parser = define_arguments()
 #class ArgHolder :
 #    pass
 #argHolder = ArgHolder()                          
-#args = parser.parse_args(namespace=argHolder                                   
+#args = parser.parse_args(namespace=argHolder                                  
 args = parser.parse_args()
 argsDict = vars(args)
 print 'les arguments passés sont :', argsDict
-def is_a_file(f):
-    try:
-        f.read()
-        return True
-    except AttributeError:
-        return False
-        
-"""
-def fromObjDictToStrDict(origDict):
-    '''
-    tranasforme tous les objets dans le dictionnaire en string
-    et pour les files, transorme le chemin relatif en chemin absolu
-    '''
-    res = dict()
-    for key, value in origDict.iteritems():
-        #if string cp normal
-        if isinstance(value, str):
-            res[key] = value
-        if is_a_file(value):
-            res[key] = value.name
-        if isinstance( value, int ):
-            res[key] = str(value)
-        else:
-            print 'type Error'    
-    return res
-newDict = fromObjDictToStrDict(argsDict)
-print newDict
-"""
+
 #PROCESS
 #PREREQUIS :ON A UN FICHIER SOL
 #ON A LES FICHIERS CLIQUE pour le bon nombre d'agents
@@ -138,10 +80,12 @@ from agentPartitioning import *
 import os
 
 def flow():
+    if argsDict['dist'] != None :
+        return
     if (argsDict['numagent'] == None) or (argsDict['numagent'] == 1 ) :
         print 'MONO AGENT, by default'
         #check if method basée est bien celle la : facultatif
-        if  argsDict['method'] != 'SOLAR-Inc-Carc'
+        if  argsDict['method'] != 'SOLAR-Inc-Carc':
             print 'Methode Mono Agent non passée, mis par défaut en mode Mono'
             argsDict['method'] = 'SOLAR-Inc-Carc'
                 
@@ -203,10 +147,14 @@ if argsDict['timeout'] != None :
 if argsDict['var'] != None :
     print argsDict['var']
     args.append('-var='+argsDict['var'])
+if argsDict['dist'] != None :
+    print argsDict['dist']
+    args.append('-dist='+argsDict['dist'])    
+    
 #adding arguments at the end because (cf CfLauncher.java ^^).
 args = args + [argsDict['infile'], GEN_PATH+argsDict['outfile']]
 
-print 'CFLAUNCHER ################################################' 
+print 'CFLAUNCHER MULTI ################################################' 
 result = jarWrapper(*args)  
 #problem : ici ON GARDE TOUTES LES TRACES D'EXÉCUTION EN MÉMOIRE, IL FAUDRAIT LES ECRIRE AU FUR ET A MESURE DANS UN FICHIER
 #ET ON RECUPEREAIT ENSUITE SEULEMENT CE QUI NOUS INTERESSERAIT
@@ -216,16 +164,37 @@ char_clauses = result
 print len(result)
 #on écrit les résultats dans un fichier pour un traitement ultérieur
 #print os.path.basename(argsDict['infile'])
-outfile_csq = os.path.basename(argsDict['infile'])[:-4] + '.csq'
+'''
+outfile_csq = os.path.basename(argsDict['infile'])[:-4] + '.multicsq.log'
 f = open(GEN_PATH+outfile_csq, 'w')
 
 for line in char_clauses :
     f.write(line+'\n')
-#f.writelines(char_clauses)
-
 f.close()
+'''
+print 'CFLAUNCHER MONO################################################'
 
+argsDict_MONO = argsDict.copy()
+argsDict_MONO['numagent'] == 1
+result = jarWrapper(*args)
+
+'''
+char_clauses = result
+outfile_csq = os.path.basename(argsDict['infile'])[:-4] + '.monocsq.log'
+f = open(GEN_PATH+outfile_csq, 'w')
+
+for line in char_clauses :
+    f.write(line+'\n')
+f.close()
+'''
+
+
+
+
+##and now checking the results between mono agent and Multi
 #and compare the result with what we should have had
+
+
 #python test.py ressources/glucolysis.sol debug_test1.csv -n 4 --var _max-4_ld-1--1 --verbose -t 10000
 ##
 #//-var=_max-4_ld-1--1 -method=DICF-PB-Token-FixedOrder-3-2-1-0 -dist=_kmet4 -verbose glucolysis.sol debugGluc.csv
